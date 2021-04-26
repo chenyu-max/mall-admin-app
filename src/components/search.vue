f1d829ee342c58fb7c3e1bfc16fb98fe8db09c62
chenyu-max <56399206+chenyu-max@users.noreply.github.com>
2021-04-26 19:55:48 +0800

    7.商品列表页面-搜索组件实现

---
+++ src/components/search.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="search-box">
+    <a-form-model layout="inline" :model="searchForm" @submit="handleSubmit" @submit.native.prevent>
+      <a-form-model-item label="检索关键字">
+        <a-input v-model="searchForm.searchWord" placeholder="查询请输入关键字">
+        </a-input>
+      </a-form-model-item>
+      <a-form-model-item label="商品类目">
+        <a-select
+          show-search
+          allowClear
+          placeholder="请选择商品类目"
+          style="width: 200px"
+          @change="handleChange"
+        >
+          <a-select-option v-for="c in categoryList" :key="c.id" :value="c.id">
+            {{ c.name }}
+          </a-select-option>
+        </a-select>
+      </a-form-model-item>
+      <a-form-model-item>
+        <a-button
+          type="primary"
+          html-type="submit"
+        >
+          搜索
+        </a-button>
+      </a-form-model-item>
+    </a-form-model>
+  </div>
+</template>
+
+<script>
+import api from '@/api/category';
+
+export default {
+  name: 'search',
+  data() {
+    return {
+      searchForm: {
+        searchWord: '',
+        category: '',
+      },
+      categoryList: [],
+    };
+  },
+  created() {
+    api.list()
+      .then((res) => {
+        this.categoryList = res.data;
+      });
+  },
+  methods: {
+    // 提交表单的时候出发的事件
+    handleSubmit() {
+      this.$emit('submit', this.searchForm);
+    },
+    handleChange(val) {
+      this.searchForm.category = val;
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+.search-box {
+  padding: 10px 30px;
+}
+</style>
--- src/views/page/productList.vue
+++ src/views/page/productList.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>产品列表</div>
+  <div class="product-list">
+    <!--  搜索  -->
+    <search @submit="searchSubmit"/>
+    <!--  表格  -->
+  </div>
 </template>
 
 <script>
+import search from '@/components/search.vue';
+
 export default {
   name: 'productList',
+  components: {
+    search,
+  },
+  methods: {
+    searchSubmit(form) {
+      window.console.log(form);
+    },
+  },
 };
 </script>
 
